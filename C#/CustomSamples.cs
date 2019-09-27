22f9e59c91dc65717e4b2e07768ddbf1cde28935
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-09-27 13:07:29 +0300

    remove usings

--- C#/CustomSamples.cs
+++ C#/CustomSamples.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Windows.Forms;
-using System.Xml.Linq;
-using DataSetSampleQueries;
 using SampleSupport;
 
 
